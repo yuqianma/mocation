83e3ef7c821c1788f4e8802688699cde68d8c82f
Yuqian Ma <mayq0422@gmail.com>
2026-02-21 18:59:30 +0800

    chore: configure Vite server settings with custom port and host

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    port: 26121,
+    host: '0.0.0.0',
+  },
   plugins: [react()],
   resolve: {
     alias: {
